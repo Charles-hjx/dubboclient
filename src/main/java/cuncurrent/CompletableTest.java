6024ec9e6527be18b921a857691ecc21417520fd
hjx <hjx@example.com>
2019-08-07 17:13:02 +0800

    多线程

---
+++ src/main/java/cuncurrent/CompletableTest.java
@@ -0,0 +1,77 @@
+package cuncurrent;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @Author: hjx
+ * @Date: 2019/8/7
+ * @Version 1.0
+ */
+public class CompletableTest {
+    public static void main(String[] args) {
+        // 创建线程池
+        ExecutorService executor =
+                Executors.newFixedThreadPool(3);
+        // 异步向电商S1询价
+        Future<Integer> f1 =
+                executor.submit(()->getPriceByS1());
+        // 异步向电商S2询价
+        Future<Integer> f2 =
+                executor.submit(()->getPriceByS2());
+        // 异步向电商S3询价
+        Future<Integer> f3 =
+                executor.submit(()->getPriceByS3());
+
+        // 获取电商S1报价并异步保存
+            executor.execute(()-> {
+                try {
+                    save(f1.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+            // 获取电商S2报价并异步保存
+            executor.execute(()-> {
+                try {
+                    save(f2.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+            // 获取电商S3报价并异步保存
+            executor.execute(()-> {
+                try {
+                    save(f3.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+
+
+    }
+
+    private static Integer getPriceByS1(){
+        return 1;
+    }
+
+    private static Integer getPriceByS2(){
+        return 2;
+    }
+
+    private static Integer getPriceByS3(){
+        return 3;
+    }
+
+    private static void save(Integer price){
+
+    }
+}
--- src/main/java/cuncurrent/MyThreadPool.java
+++ src/main/java/cuncurrent/MyThreadPool.java
@@ -1,9 +1,68 @@
 package cuncurrent;
 
+import com.sun.xml.internal.txw2.output.IndentingXMLFilter;
+import queue.BlockQueue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * @Author: hjx
  * @Date: 2019/8/7
  * @Version 1.0
  */
 public class MyThreadPool {
+    //利用阻塞队列实现生产者 消费者模式
+    BlockingQueue<Runnable> workQueue ;
+
+    //保存内部工作线程
+    List<WorkerThread> threads = new ArrayList<>();
+
+    //构造方法
+    public MyThreadPool(int poolSize, BlockingQueue<Runnable> workQueue) {
+        this.workQueue = workQueue;
+        //创建工作线程
+        for(int idx = 0; idx < poolSize; idx++){
+            WorkerThread work = new WorkerThread();
+            work.start();
+            threads.add(work);
+        }
+    }
+
+    //提交任务
+    void execute(Runnable command){
+        try {
+            workQueue.put(command);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingDeque<>(2);
+        //创建线程池
+        MyThreadPool pool = new MyThreadPool(10,workQueue);
+        pool.execute(() ->{
+            System.out.println("执行线程池");
+        });
+    }
+
+
+    class WorkerThread extends Thread{
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    Runnable task = workQueue.take();
+                    task.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
