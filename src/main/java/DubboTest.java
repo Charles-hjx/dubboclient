131db267622455d6cd79fc4d54406ea0fe207108
hjx <hjx@example.com>
2019-08-15 14:16:02 +0800

    11

--- src/main/java/DubboTest.java
+++ src/main/java/DubboTest.java
@@ -9,9 +9,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DubboTest {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-client.xml");
-        APIHello apiHello = (APIHello)context.getBean("helloService");
-        System.out.println(apiHello.sayHello());
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-client.xml");
+//        APIHello apiHello = (APIHello)context.getBean("helloService");
+//        System.out.println(apiHello.sayHello());
+
+        double a = 3.2d;
+
+        double b = 4.8d;
+
+
+        Integer c = 5;
+        System.out.println(c.doubleValue());
+
+        System.out.println(a+b);
 
     }
 }
